334b0a97ca7e42bc01970bf2ce9e36a36864b182
Damien Mortini <ddotmnn+github@gmail.com>
2018-02-12 14:14:48 +1100

    transform feedback working

--- src/main/View.js
+++ src/main/View.js
@@ -3,9 +3,14 @@ import GLProgram from "../../node_modules/dlib/gl/GLProgram.js";
 import GLMesh from "../../node_modules/dlib/gl/GLMesh.js";
 import Camera from "../../node_modules/dlib/3d/Camera.js";
 import TrackballController from "../../node_modules/dlib/3d/controllers/TrackballController.js";
+import GLVertexAttribute from "../../node_modules/dlib/gl/GLVertexAttribute.js";
+import GLVertexArray from "../../node_modules/dlib/gl/GLVertexArray.js";
+import GLBuffer from "../../node_modules/dlib/gl/GLBuffer.js";
+import GUI from "../../node_modules/dlib/gui/GUI.js";
+import Pointer from "../../node_modules/dlib/input/Pointer.js";
 
 export default class View {
-  constructor({canvas} = {}) {
+  constructor({canvas} = {canvas}) {
     this.canvas = canvas;
 
     const webGLOptions = {
@@ -13,6 +18,8 @@ export default class View {
       alpha: false,
       antialias: true
     };
+
+    this.pointer = Pointer.get(this.canvas);
 
     if(!/\bforcewebgl1\b/.test(window.location.search)) {
       this.gl = this.canvas.getContext("webgl2", webGLOptions);
@@ -25,7 +32,7 @@ export default class View {
 
     this.cameraController = new TrackballController({
       matrix: this.camera.transform,
-      distance: 5
+      distance: Math.sqrt(3)
     });
 
     this.gl.clearColor(0, 0, 0, 1);
@@ -34,6 +41,7 @@ export default class View {
 
     this.program = new GLProgram({
       gl: this.gl,
+      transformFeedbackVaryings: ["positionOut"],
       uniforms: [
         ["transform", new Matrix4()]
       ],
@@ -41,25 +49,27 @@ export default class View {
         ["start", `
           uniform mat4 projectionView;
           uniform mat4 transform;
+          uniform vec4 pointer;
 
-          in vec3 normal;
           in vec3 position;
 
-          out vec3 vNormal;
+          out vec3 positionOut;
         `],
         ["end", `
+          vec3 position = position;
+          position.xy += pointer.zw * .01 * smoothstep(0., 1., .3 - distance(position.xy, pointer.xy));
           gl_Position = projectionView * transform * vec4(position, 1.);
-          vNormal = normal;
+          gl_PointSize = ${devicePixelRatio};
+
+          positionOut = position;
         `]
       ],
       fragmentShaderChunks: [
         ["start", `
           precision highp float;
-
-          in vec3 vNormal;
         `],
         ["end", `
-          fragColor = vec4(vNormal * .5 + .5, 1.);
+          fragColor = vec4(1.);
         `]
       ]
     });
@@ -67,21 +77,50 @@ export default class View {
     this.mesh = new GLMesh({
       gl: this.gl,
       attributes: [
-        ["position", {
-          data: new Float32Array([-0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5]),
-          size: 3
-        }],
-        ["normal", {
-          data: new Float32Array([0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0]),
-          size: 3
-        }]
-      ],
-      indiceData: new Uint8Array([0, 2, 3, 0, 3, 1, 4, 6, 7, 4, 7, 5, 8, 10, 11, 8, 11, 9, 12, 14, 15, 12, 15, 13, 16, 18, 19, 16, 19, 17, 20, 22, 23, 20, 23, 21])
+        ["position", new GLVertexAttribute({
+            gl: this.gl,
+            size: 3
+          })
+        ]
+      ]
     });
+
+    this.transformFeedback = this.gl.createTransformFeedback();
+    this.gl.bindTransformFeedback(this.gl.TRANSFORM_FEEDBACK, this.transformFeedback);
   }
 
   resize(width, height) {
     this.camera.aspectRatio = width / height;
+
+    width *= .5;
+    height *= .5;
+
+    width = Math.floor(width);
+    height = Math.floor(height);
+
+    const positions = new Float32Array(width * height * 3);
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < height; j++) {
+        const id = j * width + i;
+        // positions[id * 3] = ((i / width) * 2 - 1) * this.camera.aspectRatio;
+        // positions[id * 3 + 1] = (j / height) * 2 - 1;
+        positions[id * 3] = (Math.random() * 2 - 1) * this.camera.aspectRatio;
+        positions[id * 3 + 1] = (Math.random() * 2 - 1);
+      }
+    }
+
+    this.transformFeedbackBuffer1 = new GLBuffer({
+      gl: this.gl,
+      data: positions,
+      usage: this.gl.DYNAMIC_COPY
+    });
+
+    this.transformFeedbackBuffer2 = new GLBuffer({
+      gl: this.gl,
+      data: positions,
+      usage: this.gl.DYNAMIC_COPY
+    });
+
     this.update();
   }
  
@@ -93,9 +132,32 @@ export default class View {
     
     this.program.use();
     this.program.uniforms.set("projectionView", this.camera.projectionView);
+    this.program.uniforms.set("pointer", [
+      this.pointer.normalizedCenteredFlippedY.x * this.camera.aspectRatio, 
+      this.pointer.normalizedCenteredFlippedY.y, 
+      this.pointer.velocity.x, 
+      -this.pointer.velocity.y
+    ]);
+    
+    this.mesh.attributes.get("position").buffer = this.transformFeedbackBuffer1;    
     this.program.attributes.set(this.mesh.attributes);
 
-    this.mesh.indices.buffer.bind();
-    this.mesh.draw();
+    this.transformFeedbackBuffer2.bind({
+      target: this.gl.TRANSFORM_FEEDBACK_BUFFER,
+      index: 0
+    });
+    
+    this.gl.beginTransformFeedback(this.gl.POINTS);
+    this.mesh.draw({
+      mode: this.gl.POINTS
+    });
+    this.gl.endTransformFeedback();
+    
+    this.transformFeedbackBuffer2.unbind({
+      target: this.gl.TRANSFORM_FEEDBACK_BUFFER,
+      index: 0
+    });
+
+    [this.transformFeedbackBuffer1, this.transformFeedbackBuffer2] = [this.transformFeedbackBuffer2, this.transformFeedbackBuffer1];
   }
 }
